208e096fe73d531cfe1a99175713e8f4c05c0901
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-05-17 18:27:45 +0200

    Created Character.cs
    A Character Model storing all the needed data.

---
+++ RickAndMorty/RickAndMorty/Model/Character.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RickAndMorty.Model
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string Species { get; set; }
+        public string Type { get; set; }
+        public string Gender { get; set; }
+        //public Origin Origin { get; set; }
+        //public Location Location { get; set; }
+        public string Image
+        {
+            get
+            {
+                return $"https://rickandmortyapi.com/api/character/avatar/{Id}.jpeg";
+            }
+        }
+        //public List<Episode> Episodes {get; set; }
+    }
+}
