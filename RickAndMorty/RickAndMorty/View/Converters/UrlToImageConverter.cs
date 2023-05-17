48ba81392c4bee055b1cb487cb325bdc1d682a99
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-05-17 21:15:42 +0200

    Create UrlToImageConverter.cs
    Converts an URL to an image

---
+++ RickAndMorty/RickAndMorty/View/Converters/UrlToImageConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace RickAndMorty.View.Converters
+{
+    public class UrlToImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || !(value is string))
+            {
+                return null;
+            }
+
+            var name = (string)value;
+            var uri = new Uri($"{name}");
+            var image = new BitmapImage(uri);
+
+            return image;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
