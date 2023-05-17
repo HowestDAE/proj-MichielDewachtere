32b021fae1a8c866b2ef80c18d014b1b481908ee
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-05-17 20:26:43 +0200

    Update CharacterPageVM.cs

--- RickAndMorty/RickAndMorty/ViewModel/CharacterPageVM.cs
+++ RickAndMorty/RickAndMorty/ViewModel/CharacterPageVM.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using RickAndMorty.Model;
 
 namespace RickAndMorty.ViewModel
 {
-    public class CharacterPageVM
+    public class CharacterPageVM : ObservableObject
     {
+        private Character _currentCharacter;
+        public Character CurrentCharacter
+        {
+            get { return _currentCharacter; }
+            set
+            {
+                _currentCharacter = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public CharacterPageVM()
+        {
+            CurrentCharacter = new Character()
+            {
+                Id = 1,
+                Name = "Rick Sanchez",
+                Status = "Alive",
+                Species = "Human",
+                Type = "",
+                Gender = "Male"
+            };
+        }
     }
 }
