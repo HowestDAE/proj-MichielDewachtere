273241b8856672e8a6fa1122a148fd921e42f1ff
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-05-17 21:15:25 +0200

    Create StatusToColorConverter.cs
    This converts the status of the character to a color that is used for the little dot in the viewpage

---
+++ RickAndMorty/RickAndMorty/View/Converters/StatusToColorConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace RickAndMorty.View.Converters
+{
+    public class StatusToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || !(value is string))
+                return null;
+
+            var status = (string)value;
+            var color = new SolidColorBrush(Colors.Black);
+            if (status.Equals("Alive"))
+                color.Color = Colors.Lime;
+            else if (status.Equals("Dead"))
+                color.Color = Colors.Red;
+
+            return color;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
