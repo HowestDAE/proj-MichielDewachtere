cec1409843af9f02b3a01d2a602813f0dbcc4bc0
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-05-17 23:07:40 +0200

    Update ICharacterRepository.cs
    Added a bunch of new filter functions

--- RickAndMorty/RickAndMorty/Repository/Interface/ICharacterRepository.cs
+++ RickAndMorty/RickAndMorty/Repository/Interface/ICharacterRepository.cs
@@ -6,6 +6,14 @@ namespace RickAndMorty.Repository.Interface
 {
     public interface ICharacterRepository
     {
-        Task<List<Character>> GetCharacterAsync();
+        Task<List<Character>> GetCharactersAsync();
+        Task<List<Character>> GetCharactersByNameAsync(string name);
+        Task<List<Character>> GetCharactersByStatusAsync(string status);
+        Task<List<Character>> GetCharactersBySpeciesAsync(string species);
+        Task<List<Character>> GetCharactersByTypeAsync(string type);
+        Task<List<Character>> GetCharactersByGenderAsync(string gender);
+        Task<List<Character>> GetCharactersByOriginAsync(string origin);
+        Task<List<Character>> GetCharactersByLocationAsync(string location);
+        Task<List<Character>> GetCharactersByEpisodeAsync(string episode);
     }
 }
