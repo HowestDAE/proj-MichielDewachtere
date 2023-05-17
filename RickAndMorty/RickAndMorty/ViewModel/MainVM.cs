843c5172f9d1f56f986a3e13602c49a2ed5ae786
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-05-17 23:28:51 +0200

    Create MainVM.cs
    Everything now is shown in the main xaml, we got buttons to switch between pages.

---
+++ RickAndMorty/RickAndMorty/ViewModel/MainVM.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using RickAndMorty.Model;
+using RickAndMorty.View;
+
+namespace RickAndMorty.ViewModel
+{
+    public class MainVM : ObservableObject
+    {
+        public string CommandText { get; set; }
+        public OverViewPage MainPage { get; set; }
+        public CharacterDetailPage CharacterDetailPage { get; set; }
+        public Page CurrentPage { get; set; }
+        public RelayCommand SwitchPageCommand { get; set; }
+
+        public MainVM()
+        {
+            MainPage = new OverViewPage();
+            CharacterDetailPage = new CharacterDetailPage();
+            CurrentPage = MainPage;
+
+            CommandText = "Show Details";
+
+            SwitchPageCommand = new RelayCommand(SwitchPage);
+        }
+
+        public void SwitchPage()
+        {
+            if (CurrentPage is OverViewPage)
+            {
+                Character selectedCharacter = (MainPage.DataContext as OverViewVM)?.SelectedCharacter;
+                if (selectedCharacter == null)
+                    return;
+
+                ((CharacterDetailPageVM)CharacterDetailPage.DataContext).CurrentCharacter = selectedCharacter;
+
+                CurrentPage = CharacterDetailPage;
+                OnPropertyChanged(nameof(CurrentPage));
+
+                CommandText = "Go Back";
+                OnPropertyChanged(nameof(CommandText));
+            }
+            else if (CurrentPage is CharacterDetailPage)
+            {
+                CurrentPage = MainPage;
+                OnPropertyChanged(nameof(CurrentPage));
+
+                CommandText = "Show Details";
+                OnPropertyChanged(nameof(CommandText));
+            }
+        }
+    }
+}
