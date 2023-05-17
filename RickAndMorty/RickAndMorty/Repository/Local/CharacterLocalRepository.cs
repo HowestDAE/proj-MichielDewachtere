e0aa6895b509413a9053abe4bcf3824baa098558
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-05-17 20:26:10 +0200

    Create CharacterLocalRepository.cs
    Reads from the local json file

---
+++ RickAndMorty/RickAndMorty/Repository/Local/CharacterLocalRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RickAndMorty.Model;
+using RickAndMorty.Repository.Interface;
+
+namespace RickAndMorty.Repository.Local
+{
+    public class CharacterLocalRepository : ICharacterRepository
+    {
+        private List<Character> _characters { get; set; }
+
+        public async Task<List<Character>> GetCharacterAsync()
+        {
+            if (_characters != null)
+                return _characters;
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "RickAndMorty.Resources.Data.characters.json";
+
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    var json = await reader.ReadToEndAsync();
+                    _characters = JsonConvert.DeserializeObject<List<Character>>(json);
+                    
+                    return _characters;
+                }
+            }
+
+        }
+    }
+}
