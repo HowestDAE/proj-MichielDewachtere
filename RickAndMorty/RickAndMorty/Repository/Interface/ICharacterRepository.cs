f75dfcf2edf6b441813a23943562a9d3c546f55a
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-05-17 20:25:48 +0200

    Create ICharacterRepository.cs
    Added an interface for the CharacterRepositories

---
+++ RickAndMorty/RickAndMorty/Repository/Interface/ICharacterRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using RickAndMorty.Model;
+
+namespace RickAndMorty.Repository.Interface
+{
+    public interface ICharacterRepository
+    {
+        Task<List<Character>> GetCharacterAsync();
+    }
+}
