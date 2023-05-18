b74585de0104846db04aa761f9dad87f5ff4de2b
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-05-18 16:14:23 +0200

    Characters are now stored and loaded via their indices

--- RickAndMorty/RickAndMorty/Model/Episode.cs
+++ RickAndMorty/RickAndMorty/Model/Episode.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RickAndMorty.Repository.API;
 
 namespace RickAndMorty.Model
 {
@@ -12,6 +13,23 @@ namespace RickAndMorty.Model
         public string Name { get; set; }
         public string AirDate { get; set; }
         public string EpisodeNumber { get; set; }
-        public List<Character> Characters { get; set; }
+        public List<int> CharacterIds { get; set; }
+
+        private List<Character> _characters;
+        public List<Character> Characters
+        {
+            get
+            {
+                LoadCharactersAsync();
+                return _characters;
+            }
+        }
+        private async void LoadCharactersAsync()
+        {
+            if (CharacterAPIRepository.GetInstance().NextPage == null)
+            {
+                _characters = await CharacterAPIRepository.GetInstance().GetCharactersByIdsAsync(CharacterIds);
+            }
+        }
     }
 }
