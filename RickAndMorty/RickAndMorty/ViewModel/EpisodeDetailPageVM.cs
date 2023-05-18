0839afd96040c3a6f5829793f0a2de2faf788c21
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-05-18 16:15:45 +0200

    Update EpisodeDetailPageVM.cs

--- RickAndMorty/RickAndMorty/ViewModel/EpisodeDetailPageVM.cs
+++ RickAndMorty/RickAndMorty/ViewModel/EpisodeDetailPageVM.cs
@@ -29,39 +29,39 @@ namespace RickAndMorty.ViewModel
                 Name = "Pilot",
                 AirDate = "December 2, 2013",
                 EpisodeNumber = "S01E01",
-                Characters = new List<Character>()
+                //Characters = new List<Character>()
             };
 
-            CurrentEpisode.Characters.Add(new Character()
-            {
-                Id = 1,
-                Name = "Rick Sanchez"
-            });
-            CurrentEpisode.Characters.Add(new Character()
-            {
-                Id = 2,
-                Name = "Morty Smith"
-            });
-            CurrentEpisode.Characters.Add(new Character()
-            {
-                Id = 3,
-                Name = "Beth"
-            });
-            CurrentEpisode.Characters.Add(new Character()
-            {
-                Id = 4,
-                Name = "Summer Smith"
-            });
-            CurrentEpisode.Characters.Add(new Character()
-            {
-                Id = 5,
-                Name = "Jerry Smith"
-            });
-            CurrentEpisode.Characters.Add(new Character()
-            {
-                Id = 6,
-                Name = "Someone Else idk"
-            });
+            //CurrentEpisode.Characters.Add(new Character()
+            //{
+            //    Id = 1,
+            //    Name = "Rick Sanchez"
+            //});
+            //CurrentEpisode.Characters.Add(new Character()
+            //{
+            //    Id = 2,
+            //    Name = "Morty Smith"
+            //});
+            //CurrentEpisode.Characters.Add(new Character()
+            //{
+            //    Id = 3,
+            //    Name = "Beth"
+            //});
+            //CurrentEpisode.Characters.Add(new Character()
+            //{
+            //    Id = 4,
+            //    Name = "Summer Smith"
+            //});
+            //CurrentEpisode.Characters.Add(new Character()
+            //{
+            //    Id = 5,
+            //    Name = "Jerry Smith"
+            //});
+            //CurrentEpisode.Characters.Add(new Character()
+            //{
+            //    Id = 6,
+            //    Name = "Someone Else idk"
+            //});
         }
 
 	}
