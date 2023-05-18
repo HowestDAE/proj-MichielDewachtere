2770d7cf4ab65ae2eafe7d3300cb65c33e7ec759
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-05-18 16:13:40 +0200

    Added repositories for the Episode and Location

---
+++ RickAndMorty/RickAndMorty/Repository/API/EpisodeAPIRepository.cs
@@ -0,0 +1,129 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using RickAndMorty.Model;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System;
+using System.Linq;
+using System.Net;
+
+namespace RickAndMorty.Repository.API
+{
+    public class EpisodeAPIRepository
+    {
+        private List<Episode> _episodes { get; set; } = new List<Episode>();
+        public Uri NextPage = new Uri("https://rickandmortyapi.com/api/episode");
+
+        // Singleton instance
+        private static EpisodeAPIRepository _instance;
+
+        // Private constructor to prevent external instantiation
+        private EpisodeAPIRepository()
+        {
+        }
+
+        // Public static method to access the singleton instance
+        public static EpisodeAPIRepository GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new EpisodeAPIRepository();
+            }
+
+            return _instance;
+        }
+
+        public async Task<List<Episode>> GetEpisodesAsync()
+        {
+            if (NextPage == null)
+                return _episodes;
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = NextPage;
+
+                HttpResponseMessage response = await client.GetAsync("");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseData = await response.Content.ReadAsStringAsync();
+                    var pageInfo = JObject.Parse(responseData)/*["results"]*/;
+                    var episodes = pageInfo.Value<JArray>("results");
+                    var episodeList = await DeserializeEpisodes(episodes);
+                    if (episodeList != null)
+                        _episodes.AddRange(episodeList);
+
+                    var adress = pageInfo.Value<JObject>("info").Value<string>("next");
+                    if (adress != null)
+                    {
+                        NextPage = new Uri(adress);
+                        await GetEpisodesAsync();
+                    }
+                    else
+                    {
+                        NextPage = null;
+                    }
+
+                    return _episodes;
+                }
+            }
+
+            return null;
+        }
+
+        public async Task<List<Episode>> DeserializeEpisodes(JArray array)
+        {
+            var episodes = new List<Episode>();
+
+            foreach (var episode in array)
+            {
+                var newEpisode = new Episode()
+                {
+                    Id = episode.Value<int>("id"),
+                    Name = episode.Value<string>("name"),
+                    AirDate = episode.Value<string>("air_date"),
+                    EpisodeNumber = episode.Value<string>("episode"),
+                    CharacterIds = new List<int>()
+                };
+
+                var characters = episode.Value<JArray>("characters");
+                if (characters != null)
+                {
+                    var characterIds = new List<int>();
+                    foreach (var character in characters)
+                    {
+                        var address = character.Value<string>();
+
+                        // Find the index of the first forward slash after the word "character"
+                        int slashIndex = address.IndexOf("/character/") + "/character/".Length;
+
+                        // Extract the substring starting from the index of the first forward slash
+                        string idString = address.Substring(slashIndex);
+
+                        // Parse the extracted substring to an integer
+                        int id;
+                        if (int.TryParse(idString, out id))
+                        {
+                            characterIds.Add(id);
+                        }
+                    }
+
+                    newEpisode.CharacterIds.AddRange(characterIds);
+                }
+
+
+                episodes.Add(newEpisode);
+            }
+
+            return episodes;
+        }
+
+        public async Task<List<Episode>> GetEpisodesByIdsAsync(List<int> ids)
+        {
+            var episodes = await GetEpisodesAsync();
+            var filteredEpisodes = episodes.Where(e => ids.Contains(e.Id)).ToList();
+            return filteredEpisodes;
+        }
+    }
+}
---
+++ RickAndMorty/RickAndMorty/Repository/API/LocationAPIRepository.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using RickAndMorty.Model;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System;
+
+namespace RickAndMorty.Repository.API
+{
+    public class LocationAPIRepository
+    {
+        private List<Location> _locations { get; set; } = new List<Location>();
+        private Uri _nextPage = new Uri("https://rickandmortyapi.com/api/location");
+
+        // Singleton instance
+        private static LocationAPIRepository _instance;
+
+        // Private constructor to prevent external instantiation
+        private LocationAPIRepository()
+        {
+        }
+
+        // Public static method to access the singleton instance
+        public static LocationAPIRepository GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new LocationAPIRepository();
+            }
+
+            return _instance;
+        }
+
+        public async Task<List<Location>> GetLocationsAsync()
+        {
+            if (_nextPage == null)
+                return _locations;
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = _nextPage;
+
+                HttpResponseMessage response = await client.GetAsync("");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseData = await response.Content.ReadAsStringAsync();
+                    var pageInfo = JObject.Parse(responseData)/*["results"]*/;
+                    var locations = pageInfo.Value<JArray>("results");
+                    var locationList = JsonConvert.DeserializeObject<List<Location>>(locations.ToString());
+                    if (locationList != null)
+                        _locations.AddRange(locationList);
+
+                    var adress = pageInfo.Value<JObject>("info").Value<string>("next");
+                    if (adress != null)
+                    {
+                        _nextPage = new Uri(adress);
+                        await GetLocationsAsync();
+                    }
+
+                    return _locations;
+                }
+            }
+
+            return null;
+        }
+    }
+}
